531e6978b55d7a408b4c8061337f64e889b64d4e
NightDev19 <sherwintajan143@gmail.com>
2025-11-19 15:54:06 +0800

    feat(Learning TS) : Added to -> Data Types Added -> Explicit and Inference Type

---
+++ Data_Types/data.types.explicit.ts
@@ -0,0 +1,8 @@
+// Function with Explicit parameter and return types
+function greet(name:string){ // The explicit here is the name:string
+	return `Hello ${name}`
+}
+
+// Typescript will ensure you pass the correct argument type
+greet("Kj"); // Pass
+// greet(23); // Error :Argument of type 'number' is not assignable to parameter of type 'string'
---
+++ Data_Types/data.types.inference.ts
@@ -0,0 +1,12 @@
+// Basic Type Inference
+// Typescript inferce  as
+//'string'
+let username = "Kj";
+// 'number'
+let age = 23;
+// 'boolean[]'
+let flags = [true, false, true];
+// return type as 'number'
+function add(a: number, b: number) {
+  return a + b;
+}
