648e6e69ad28ce98166f7d556db7dcab52e1aea6
NightDev19 <sherwintajan143@gmail.com>
2025-11-19 16:30:59 +0800

    Feat(Learning TS) : Updated to -> Data Types Updated -> added console.log for data output

--- Data_Types/data.types.explicit.ts
+++ Data_Types/data.types.explicit.ts
@@ -1,8 +1,9 @@
 // Function with Explicit parameter and return types
-function greet(name:string){ // The explicit here is the name:string
-	return `Hello ${name}`
+function greet(name: string) {
+  // The explicit here is the name:string
+  return `Hello ${name}`;
 }
 
 // Typescript will ensure you pass the correct argument type
-greet("Kj"); // Pass
+console.log(greet("Kj")); // Pass
 // greet(23); // Error :Argument of type 'number' is not assignable to parameter of type 'string'
--- Data_Types/data.types.inference.ts
+++ Data_Types/data.types.inference.ts
@@ -2,11 +2,16 @@
 // Typescript inferce  as
 //'string'
 let username = "Kj";
+console.log(username);
 // 'number'
 let age = 23;
+console.log(age);
 // 'boolean[]'
 let flags = [true, false, true];
+console.log(flags);
 // return type as 'number'
 function add(a: number, b: number) {
   return a + b;
 }
+
+console.log(add(1, 2));
